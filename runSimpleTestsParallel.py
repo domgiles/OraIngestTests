9a8db58aedb04c97f835d04087d72d04b0f64fd2
domgiles <dominic.giles@gmail.com>
2017-09-05 09:38:11 +0100

    Fixes for JVM results reporting

--- runSimpleTestsParallel.py
+++ runSimpleTestsParallel.py
@@ -2,11 +2,12 @@ from __future__ import print_function
 
 import argparse
 import logging
+import re
 import subprocess
 import sys
-from threading import Thread
-import re
 import time
+from threading import Thread
+
 from prettytable import PrettyTable
 
 DEFAULT_BATCH_SIZE = 1
@@ -19,6 +20,8 @@ path_to_executable = 'java -jar /Users/dgiles/java/SimpleOraTest/out/artifacts/S
 runCommand = "{path_to_command} -u {user_name} -p {pass_word} -cs {connect_string} -bs {batch_size} -cf {commit_size} -rc {row_count} -tc {thread_count}"
 
 results = []
+process_results = []
+
 
 def print_results(results, description):
     cols = description
@@ -40,16 +43,17 @@ def set_logging(level):
 
 
 def executeCommand(command):
-    start =time.time()
     p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
     (output, err) = p.communicate()
-    end= time.time()
     s = re.findall("(Rows Inserted per sec[\s]*)([0-9,]*)", output.decode("utf-8"))
     t = re.findall("(Actual Rows Generated[\s]*)([0-9,]*)", output.decode("utf-8"))
-    rows_processed = s[0][1]
-    rows_inserted = t[0][1]
-    time_taken = end - start
-    results.append((rows_processed, rows_inserted, time_taken,))
+    c = re.findall("(Connection Time[\s]*)([0-9.]*)", output.decode("utf-8"))
+    i = re.findall("(Data Generation Time[\s]*)([0-9.]*)", output.decode("utf-8"))
+    rows_processed = int(s[0][1].replace(',', ''))
+    rows_inserted = int(t[0][1].replace(',', ''))
+    connection_time = float(c[0][1])
+    insertion_time = float(i[0][1])
+    process_results.append((connection_time, rows_inserted, insertion_time, rows_processed,))
 
 
 def run_tests(path_to_executable, username, password, connect_string, row_counts, commit_sizes, batch_sizes, thread_counts, processes):
@@ -74,15 +78,28 @@ def run_tests(path_to_executable, username, password, connect_string, row_counts
         for thread in threads:
             thread.join()
         end = time.time()
-        results=[(processes[0],
-                  int(row_counts[0])*int(processes[0]),
-                  batch_sizes[0],
-                  commit_sizes[0],
-                  thread_counts[0],
-                  "{0:,.2f}".format(end - start),
-                  "{0:,.0f}".format((int(row_counts[0])*int(processes[0]))/(end -start)),)]
-        description=("JVMs Started","Total Rows Inserted", "Batch Size", "Commit Size", "Threads", "Time Taken (secs)", "Rows/sec inserted")
-        print_results(results,description)
+        insertion_time, connection_time, rows_inserted, rows_processed, max_insertion_time = 0, 0, 0, 0, 0
+        for ct, ri, it, rp in process_results:
+            insertion_time += it
+            connection_time += ct
+            rows_inserted += ri
+            rows_processed += rp
+            max_insertion_time = max(max_insertion_time, it)
+        logging.debug("insertion time = {}, connection time = {}, rows_inserted = {}, rows_processed = {}, max_insertion_time = {}".format(insertion_time, connection_time, rows_inserted, rows_processed, max_insertion_time))
+        results = [(processes[0],
+                    int(row_counts[0]) * int(processes[0]),
+                    batch_sizes[0],
+                    commit_sizes[0],
+                    thread_counts[0],
+                    "{0:,.2f}".format(end - start),
+                    "{0:,.2f}".format(insertion_time),
+                    "{0:,.0f}".format(rows_processed / max_insertion_time),)]
+        description = ("JVMs Started", "Total Rows Inserted", "Batch Size", "Commit Size", "Threads", "Real Time Taken", "Total Insert Time", "Rows/sec Inserted")
+        print_results(results, description)
+        print("Individual JVM Results")
+        description = ("Connection Time", "Rows Processed", "Insert Time", "Rows/sec Inserted",)
+        print_results(process_results, description)
+        logging.debug("Completed test in {}".format(end - start))
 
     except Exception as e:
         print("Unable to run test : {}".format(e.message), file=sys.stderr)
